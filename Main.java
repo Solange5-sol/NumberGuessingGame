71d3c60c299f46deb84189e04704c39a835ebf79
Solange5-sol <usolange195@gmail.com>
2025-09-18 07:49:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,19 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game();       
         Player player = new Player(); 
-
         boolean guessedRight = false;
         int tries = 0;
-
         System.out.println("Number Guessing Game!");
         System.out.println("Pick a number between 1 and 100.");
-
         while (!guessedRight && tries < 6) {
             int guess = player.guessNumber();
             guessedRight = game.checkGuess(guess);
             tries++;
         }
-
         if (!guessedRight) {
             System.out.println("Game over! You used all your tries.");
         } else {
